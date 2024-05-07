95c7d3c65d5b2f8fd2b03555aeff3bf6684b0f03
Annatar <itsannatar@gamil.com>
2024-05-07 20:06:05 +0330

    feat: persist comments on local storage

--- src/_features/Comments/store/CommentStore.tsx
+++ src/_features/Comments/store/CommentStore.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable fp/no-loops */
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import initComments from "@/_features/Comments/data/user.json";
 
@@ -41,97 +42,104 @@ const removeRecursive = (commentsArr: Comment[], parentId: number) => {
   });
 };
 
-export const useCommentStore = create<CommentType>((set) => ({
-  comments: initComments,
-  addComment: (comment) => {
-    if (comment.body.length >= 2) {
-      set((state) => ({
-        comments: [comment, ...state.comments],
-      }));
-    }
-  },
-  removeComment: (id) => {
-    set((state) => ({
-      comments: removeRecursive(state.comments, id),
-    }));
-  },
-  updateReplies: (comment, id) => {
-    set((state) => {
-      const updatedComment = (replies: Comment[]) =>
-        replies.map((c) => {
-          if (c.id === id) {
-            c.replies = [...c.replies, comment];
-          } else if (c.replies) {
-            c.replies = updatedComment(c.replies);
+export const useCommentStore = create(
+  persist<CommentType>(
+    (set) => ({
+      comments: initComments,
+      addComment: (comment) => {
+        if (comment.body.length >= 2) {
+          set((state) => ({
+            comments: [comment, ...state.comments],
+          }));
+        }
+      },
+      removeComment: (id) => {
+        set((state) => ({
+          comments: removeRecursive(state.comments, id),
+        }));
+      },
+      updateReplies: (comment, id) => {
+        set((state) => {
+          const updatedComment = (replies: Comment[]) =>
+            replies.map((c) => {
+              if (c.id === id) {
+                c.replies = [...c.replies, comment];
+              } else if (c.replies) {
+                c.replies = updatedComment(c.replies);
+              }
+
+              return c;
+            });
+          return { comments: updatedComment(state.comments) };
+        });
+      },
+      editComments: (message, id) => {
+        set((state) => {
+          const editCm = (commentsForEdit: Comment[], targetId: number) => {
+            return commentsForEdit.map((c) => {
+              if (c.id === targetId) {
+                c = { ...c, body: message };
+              } else if (c.replies) {
+                c.replies = editCm(c.replies, targetId);
+              }
+
+              return c;
+            });
+          };
+
+          const updatedComment = editCm(state.comments, id);
+          return { comments: updatedComment };
+        });
+      },
+      likes: [],
+      toggleLike: (id) => {
+        set((state) => {
+          const isLiked = state.likes.find((like) => like.id === id);
+
+          if (isLiked) {
+            return {
+              ...state,
+              likes: state.likes.filter((like) => like.id !== id),
+            };
+          } else {
+            return { ...state, likes: [...state.likes, { id, count: 1 }] };
+          }
+        });
+      },
+      countLikes: (likes, id) => {
+        return likes.reduce((totalLikes, like) => {
+          if (like.id === id) {
+            return totalLikes + like.count;
           }
 
-          return c;
-        });
-      return { comments: updatedComment(state.comments) };
-    });
-  },
-  editComments: (message, id) => {
-    set((state) => {
-      const editCm = (commentsForEdit: Comment[], targetId: number) => {
-        return commentsForEdit.map((c) => {
-          if (c.id === targetId) {
-            c = { ...c, body: message };
-          } else if (c.replies) {
-            c.replies = editCm(c.replies, targetId);
+          return totalLikes;
+        }, 0);
+      },
+      getUsername: (cms, id) => {
+        const findUsername = (cm: Comment[]): string | undefined => {
+          for (const comment of cm) {
+            if (comment.id === id) {
+              return comment.name;
+            }
+
+            if (comment.replies && comment.replies.length > 0) {
+              const replyUsername = findUsername(comment.replies);
+
+              if (replyUsername) {
+                return replyUsername;
+              }
+            }
           }
 
-          return c;
-        });
-      };
-
-      const updatedComment = editCm(state.comments, id);
-      return { comments: updatedComment };
-    });
-  },
-  likes: [],
-  toggleLike: (id) => {
-    set((state) => {
-      const isLiked = state.likes.find((like) => like.id === id);
-
-      if (isLiked) {
-        return {
-          ...state,
-          likes: state.likes.filter((like) => like.id !== id),
+          return undefined;
         };
-      } else {
-        return { ...state, likes: [...state.likes, { id, count: 1 }] };
-      }
-    });
-  },
-  countLikes: (likes, id) => {
-    return likes.reduce((totalLikes, like) => {
-      if (like.id === id) {
-        return totalLikes + like.count;
-      }
 
-      return totalLikes;
-    }, 0);
-  },
-  getUsername: (cms, id) => {
-    const findUsername = (cm: Comment[]): string | undefined => {
-      for (const comment of cm) {
-        if (comment.id === id) {
-          return comment.name;
-        }
-
-        if (comment.replies && comment.replies.length > 0) {
-          const replyUsername = findUsername(comment.replies);
-
-          if (replyUsername) {
-            return replyUsername;
-          }
-        }
-      }
-
-      return undefined;
-    };
-
-    const username = findUsername(cms);
-    return username;
-  },
-}));
+        const username = findUsername(cms);
+        return username;
+      },
+    }),
+    {
+      name: "comments-storage",
+    },
+  ),
+);
