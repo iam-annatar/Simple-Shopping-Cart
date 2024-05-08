fd6874bd2ac444807390a000fbeb80e728d90ecb
Annatar <itsannatar@gamil.com>
2024-05-08 21:57:08 +0330

    refactor: modify search functionality and add debounce hook

--- src/_features/ShoppingStore/page/StorePage.tsx
+++ src/_features/ShoppingStore/page/StorePage.tsx
@@ -2,12 +2,12 @@ import { useSearchContext } from "@/_features/Search/hook/useSearchContext";
 import { ShoppingItems } from "@/_features/ShoppingStore/components/ShoppingItems";
 
 export const Store = () => {
-  const { searchValue, filterItems } = useSearchContext();
+  const { filterItems } = useSearchContext();
   return (
     <>
       <h1 className="my-4 text-xl font-bold">Store</h1>
       <div className="mb-8 grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {filterItems(searchValue).map((item) => (
+        {filterItems().map((item) => (
           <ShoppingItems key={item.id} {...item} />
         ))}
       </div>
