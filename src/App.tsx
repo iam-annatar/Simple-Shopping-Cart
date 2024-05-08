72c946735242187c44854a0c9a3a30e1e613be77
Annatar <itsannatar@gamil.com>
2024-05-08 16:44:23 +0330

    refactor: modify imports in main and app.tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -7,41 +7,27 @@ import { About } from "./_features/About/page/AboutPage";
 import { Home } from "./_features/Home/page/HomePage";
 import { Navbar } from "./_features/Navbar/components/Navbar";
 import { ProductPage } from "./_features/Product/page/ProductPage";
-import { SearchContext } from "./_features/Search/context/SearchContext";
-import { ShoppingCartContext, Store } from "./_features/ShoppingStore";
-import {
-  LikeContext,
-  WishListContext,
-  WishListPage,
-} from "./_features/WishList";
-import { ThemeProvider } from "./shared/context/ThemeProvider";
+import { Store } from "./_features/ShoppingStore";
+import { WishListPage } from "./_features/WishList";
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="Theme">
-      <ShoppingCartContext>
-        <SearchContext>
-          <LikeContext>
-            <WishListContext>
-              <Navbar />
-              <div className="container">
-                <Routes>
-                  <Route path="/">
-                    <Route index element={<Navigate to="/store" replace />} />
-                  </Route>
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/store" element={<Store />} />
-                  <Route path="/store/:productId" element={<ProductPage />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/wishlist" element={<WishListPage />} />
-                </Routes>
-              </div>
-              <Toaster duration={2000} richColors closeButton />
-            </WishListContext>
-          </LikeContext>
-        </SearchContext>
-      </ShoppingCartContext>
-    </ThemeProvider>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/">
+            <Route index element={<Navigate to="/store" replace />} />
+          </Route>
+          <Route path="/home" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/store/:productId" element={<ProductPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/wishlist" element={<WishListPage />} />
+        </Routes>
+      </div>
+      <Toaster duration={2000} richColors closeButton />
+    </>
   );
 };
 
