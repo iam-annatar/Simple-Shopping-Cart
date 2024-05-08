2c8b908dabe58bd6eadda0f79c0b3a54072ef085
Annatar <itsannatar@gamil.com>
2024-05-08 16:44:23 +0330

    refactor: modify imports in main and app.tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,25 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { SearchContext } from "./_features/Search/context/SearchContext";
-import { ShoppingCartContext } from "./_features/ShoppingStore/context/ShoppingCartContext";
-import { LikeContext } from "./_features/WishList/context/LikeContext";
-import { WishListContext } from "./_features/WishList/context/WishListContext";
+import { ShoppingCartContext } from "./_features/ShoppingStore";
+import { LikeContext, WishListContext } from "./_features/WishList";
 import App from "./App";
+import { ThemeProvider } from "./shared/context/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ShoppingCartContext>
-        <SearchContext>
-          <LikeContext>
-            <WishListContext>
-              <App />
-            </WishListContext>
-          </LikeContext>
-        </SearchContext>
-      </ShoppingCartContext>
+      <ThemeProvider defaultTheme="system" storageKey="Theme">
+        <ShoppingCartContext>
+          <SearchContext>
+            <LikeContext>
+              <WishListContext>
+                <App />
+              </WishListContext>
+            </LikeContext>
+          </SearchContext>
+        </ShoppingCartContext>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
